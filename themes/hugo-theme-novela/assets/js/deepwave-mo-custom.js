3dfc15fb454e5c6e80a18f5cd724c9455084231b
TWWCT <work@goeoeck.eu>
2022-04-11 19:55:20 +0200

    MVP ScrollMagic
    Proof of concept ScrollMagic

--- themes/hugo-theme-novela/assets/js/deepwave-mo-custom.js
+++ themes/hugo-theme-novela/assets/js/deepwave-mo-custom.js
@@ -3,9 +3,132 @@ console.log('deepwave-mo-custom.js init')
 
 document.addEventListener('DOMContentLoaded', function(e){
     
-    handleMOClicks();
+    handleMOClicks()
+    handleMOScrollMagic()
     
 })
+
+// #################################################################################
+// #################################################################################
+
+function handleMOScrollMagic(){
+
+    //
+    // init ScrollMagic controller
+    //
+
+    var controller = new ScrollMagic.Controller({
+        globalSceneOptions: {
+            triggerHook: 'onLeave', // onEnter // onCenter // onLeave
+            reverse: true
+        }
+    });
+
+
+    /// EASING FUNCTIONS
+    // t = time, b = beginning value, c = change in value, d = duration
+    function easeInOutQuad (t, b, c, d) {
+        if ((t /= d / 2) < 1) return c / 2 * t * t + b;
+        return -c / 2 * ((--t) * (t - 2) - 1) + b;
+    }
+    function easeInOutBack (t, b, c, d) {
+        let s = 1.70158
+        //if (s == undefined) s = 1.70158;
+        if ((t /= d / 2) < 1) return c / 2 * (t * t * (((s *= (1.525)) + 1) * t - s)) + b;
+        return c / 2 * ((t -= 2) * t * (((s *= (1.525)) + 1) * t + s) + 2) + b;
+    }
+    function easeInOutBackS (t, b, c, d, s) {
+        //let s = 1.70158
+        //if (s == undefined) s = 1.70158;
+        if ((t /= d / 2) < 1) return c / 2 * (t * t * (((s *= (1.525)) + 1) * t - s)) + b;
+        return c / 2 * ((t -= 2) * t * (((s *= (1.525)) + 1) * t + s) + 2) + b;
+    }
+    function easeInOutExpo (t, b, c, d) {
+        if (t == 0) return b;
+        if (t == d) return b + c;
+        if ((t /= d / 2) < 1) return c / 2 * Math.pow(2, 10 * (t - 1)) + b;
+        return c / 2 * (-Math.pow(2, -10 * --t) + 2) + b;
+    }
+    
+
+    //
+    // MO ScrollMagic
+    //
+
+    /// ERFASSE ALLE MOs
+    let mos = document.querySelectorAll('.mo-section')
+
+    if( mos.length > 0 ){
+
+        /// FÜR JEDE MO SETUP SCROLLMAGIC SCENE
+        mos.forEach(function(entry, index){
+            
+            // Animated elements
+            let pOriginal = entry.querySelector('.mo-original p')
+
+            
+            // Scene ID
+            var my_scene_id = entry.getAttribute('id')
+            var scene_selector = '#' + my_scene_id
+
+            // Scene Offset
+            let scene_offset = 0 // Startpunkt der Scene in Pixel vom oberen Rand
+
+            // Scene Duration
+            let scene_duration = 300 // Dauer der Scene in Pixel
+
+            let scene_vars = {
+                triggerElement: scene_selector,
+                duration: scene_duration,
+                offset: scene_offset
+            }
+
+            createAnimationScene(my_scene_id, scene_vars)
+
+
+            /// FUNCTION: CREATE SCROLLMAGIC SCENE
+            function createAnimationScene(scene_id, scene_vars){
+
+                /// ERSTELLE SCROLLMAGIC SCENE
+                let scene = {}
+                scene[scene_id] = new ScrollMagic.Scene(scene_vars)
+                    .setPin(scene_vars.triggerElement)
+                    //.addIndicators() // DEBUG
+                    .addTo(controller)
+
+                runAnimationScene(scene, scene_id)
+            }
+
+
+            /// FUNCTION: RUN SCROLLMAGIC SCENE
+            function runAnimationScene(scene, scene_id){
+                /// STARTE SCROLLMAGIC SCENE
+
+                scene[scene_id].on('enter', function(event){
+                    // Action here
+                    console.log('entering')
+                })
+                
+                scene[scene_id].on('progress', function(event){
+                    //let padding = (50 * event.progress) + 'px'
+                    let padding = easeInOutQuad((scene_duration * event.progress), 25.6, 60, scene_duration) + 'px'
+                    pOriginal.style.setProperty('padding-bottom', padding)
+                })
+
+                scene[scene_id].on('leave', function(event){
+                    // Action here
+                    console.log('leaving')
+                })
+            }
+
+        }) 
+    } // END — MO ScrollMagic
+    
+}
+
+
+// #################################################################################
+// #################################################################################
 
 function handleMOClicks(){
     // Find all MOs
